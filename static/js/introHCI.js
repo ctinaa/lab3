24a203ed9de7b140bcf902447a75b98f6b204655
Christina Cook <cfcook@ucsd.edu>
2017-01-26 10:35:14 -0800

    still changing js, but button works

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -16,3 +16,15 @@ function initializePage() {
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+
+function projectClick(e){
+	e.preventDefault(); 
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
